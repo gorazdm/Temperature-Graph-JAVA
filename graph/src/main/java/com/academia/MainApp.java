b52303e1910449159e579179c15a8308ad5401d0
gorazdm <82107203+gorazdm@users.noreply.github.com>
2021-05-18 13:45:02 +0200

    Add files via upload

---
+++ graph/src/main/java/com/academia/MainApp.java
@@ -0,0 +1,13 @@
+package com.academia;
+
+/**
+ * Hello world!
+ *
+ */
+public class MainApp {
+
+    static MainWindow window;
+    public static void main( String[] args ) {   
+        window = new MainWindow("Unit 29 - Vaja 1");
+    }
+}
